c98a1245611f7e55d5423b2a6401ab7b79f3b3eb
MarcosWAlmeida <marcos.sor@gmail.com>
2020-07-04 20:26:19 -0300

    finalizacao do website

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const pics = document.querySelectorAll('.receitas')
 for (let pic of pics) {
     pic.addEventListener("click", function(){
         const imgId= pic.getAttribute("id")
+        const title = pic.querySelector(".recipe_title").innerHTML
+        const author = pic.querySelector(".recipe_author").innerHTML
         modalOverlay.classList.add('active')
         modalOverlay.querySelector("img").src = (`/assets/${imgId}`)
+        modalOverlay.querySelector('.modal_title').innerHTML = `${title}`
+        modalOverlay.querySelector('.modal_author').innerHTML = `${author}`
     })
 }
 
