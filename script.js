78719dbb5d2efccfa52304418b4ec3f4ad1e1df3
MarcosWAlmeida <marcos.sor@gmail.com>
2020-07-03 23:48:20 -0300

    Finalizacao modal

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const modalOverlay = document.querySelector('.modal_overlay');
 
 const pics = document.querySelectorAll('.receitas')
 
+
 for (let pic of pics) {
     pic.addEventListener("click", function(){
+        const imgId= pic.getAttribute("id")
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("img").src = (`/assets/${imgId}`)
     })
 }
 
 document.querySelector('.close-modal').addEventListener("click",function(){
         modalOverlay.classList.remove('active');
 })
+
